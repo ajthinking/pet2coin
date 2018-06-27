e59c1d7846cd658cab4ff0d8bbb0ab3397e107a8
Anders Jürisoo <jurisoo@hotmail.com>
2018-06-27 15:24:13 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/app.css" />
+    </head>
+    <body>
+        <div class="hero">
+            <div class="heading">
+                <h1> Pet 2 Coin </h1>
+                <h3> - Give your pet a coin - </h3>
+            </div>
+            <p> Its time to stop thinking about yourself. Get a coin for your belowed pet. Not tomorow, not later, but right NOW. </p>
+            <p> Then, go get the t-shirt to promote your pets coin. </p>
+            <div class="actions">
+                    <button>Get started!</button><button>Browse other pet coins</button>
+            </div>
+            
+            <img src="img/morphyCoinTshirt.png">
+        </div>
+    </body>
+</html>
